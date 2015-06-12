491abd4ea19d7dbac4ed8f536c0bbf00db0374e6
dhe <dhe@instar.cz>
2015-06-12 13:56:58 +0200

    aktualizace - pravidla, propozice, kontakty

--- app/presenters/PravidlaPresenter.php
+++ app/presenters/PravidlaPresenter.php
@@ -8,6 +8,6 @@ class PravidlaPresenter extends BasePresenter
     {
         $this->template->pageTitle = '„RB“VL - Pravidla';
         $this->template->pageDesc = '„Region Beskydy“ volejbalová liga - Pravidla soutěže';
-        $this->template->pageHeading = 'Pravidla „Region Beskydy“ volejbalové ligy<br />5. ročník (2013 - 2014)';
+        $this->template->pageHeading = 'Pravidla „Region Beskydy“ volejbalové ligy<br />7. ročník (2015 - 2016)';
     }
 }
