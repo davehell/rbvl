e3ec022d5ee1605c935f28bdd6cac9ce77e0f5dc
dhe <dhe@instar.cz>
2015-06-12 09:04:26 +0200

    aktualizace - default

--- app/presenters/DefaultPresenter.php
+++ app/presenters/DefaultPresenter.php
@@ -7,8 +7,13 @@ class DefaultPresenter extends BasePresenter
     public function renderDefault()
     {
         $this->template->pageTitle = '„Region Beskydy“ volejbalová liga';
-        $this->template->pageHeading = '„Region Beskydy“ volejbalová liga';
+        $this->template->pageHeading = '';
         $this->template->pageDesc = '„Region Beskydy“ volejbalová liga je dlouhodobá soutěž hrající se ve Frýdku-Místku.';
         $this->template->scripts = array('lightbox');
+
+        $this->template->text = '';
+        $stranky = new Stranky;
+        $stranka = $stranky->findByNazev('uvod')->fetch();
+        if($stranka) $this->template->text = $stranka->text;
     }
 }
