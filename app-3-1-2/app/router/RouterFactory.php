60dac9fe00cbcb05efbf4e50f31e411ac73df38e
David Hellebrand <david.hellebrand@elvac.eu>
2021-04-04 17:40:17 +0200

    aktualizace routovani

--- app-3-1-2/app/router/RouterFactory.php
+++ app-3-1-2/app/router/RouterFactory.php
@@ -2,41 +2,38 @@
 
 namespace App;
 
-use Nette;
-use Nette\Application\Routers\Route;
 use Nette\Application\Routers\RouteList;
-
+use Nette\Application\Routers\Route;
 
 final class RouterFactory
 {
-	use Nette\StaticClass;
-
 	/**
 	 * @return Nette\Application\IRouter
 	 */
-	public static function createRouter()
-	{
-		$router = new RouteList;
-		$router[] = new Route('index.php', 'Default:default', Route::ONE_WAY);
-		$router[] = new Route('<presenter>/<action>[/<id>]', array(
-		    'presenter' => array(
-		        Route::VALUE => 'Default',
-		        Route::FILTER_TABLE => array(
-		            // řetězec v URL => presenter
-		            'ke-stazeni' => 'Download',
-		            'turnaje-akce' => 'Akce'
-		        ),
-		    ),
-		    'action' => array(
-		        Route::VALUE => 'default',
-		        Route::FILTER_TABLE => array(
-		            'pridat' => 'add',
-		            'upravit' => 'edit',
-		            'smazat' => 'delete',
-		        ),
-		    ),
-		    'id' => NULL,
-		));
-		return $router;
-	}
+    public static function createRouter(): RouteList
+    {
+        $router = new RouteList;
+        $router->addRoute('index.php', 'Default:detail', $router::ONE_WAY);
+        $router->addRoute('<presenter>/<action>[/<id>]', [
+            'presenter' => [
+                Route::VALUE => 'Default',
+                Route::FILTER_TABLE => [
+                    // řetězec v URL => presenter
+                    'ke-stazeni' => 'Download',
+                    'turnaje-akce' => 'Akce'
+                ],
+            ],
+            'action' => [
+                Route::VALUE => 'default',
+                Route::FILTER_TABLE => [
+                    'pridat' => 'add',
+                    'upravit' => 'edit',
+                    'smazat' => 'delete'
+                ],
+            ],
+            'id' => NULL
+        ]);
+
+        return $router;
+    }
 }
